12e3ba00c86c6dcda4ca6aa6ca2f75aabcb5c75b
IgrisUVG <igris@ushivangoga.ru>
2017-05-03 18:32:59 +0300

    menu.min.js

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-  $(function () { 
-    $('.nav li').click(function () {
-			$('*').removeClass('verbergend');
-			$(this).toggleClass("active");
-			$(this).siblings(".nav li").removeClass("active");
-      $('body,html').animate({
-				scrollTop: 0
-      }, 800);
-			window.location=$(this).find("a").attr("href").attr("target");
-      return false;
+$(document).ready(function () {
+    $(function () {
+        $('.nav li').click(function () {
+            $('*').removeClass('verbergend');
+            $(this).toggleClass("active");
+            $(this).siblings(".nav li").removeClass("active");
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            window.location = $(this).find("a").attr("href").attr("target");
+            return false;
+        });
     });
-  });
-  $('.navR a').click(function() {
-		$(".nav li").removeClass("active");
-    $('*').removeClass('verbergend');
-		$(this).addClass('verbergend');
-  }) 
+    
+    $('.navR a').click(function () {
+        $(".nav li").removeClass("active");
+        $('*').removeClass('verbergend');
+        $(this).addClass('verbergend');
+    })
 });
