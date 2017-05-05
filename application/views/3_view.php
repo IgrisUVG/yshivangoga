bbbc1d020a71a6d16057ce6082d3ae61e6ef4e71
IgrisUVG <igris@ushivangoga.ru>
2017-05-05 16:33:48 +0300

    4_dushambe.

--- application/views/3_view.php
+++ application/views/3_view.php
@@ -7,5 +7,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WZT556');</script>
 <!-- End Google Tag Manager -->
-<center>
-<iframe src="../../mus/alb/uhim3.html" width="100%" height="1470" frameborder="0"></iframe>
+<iframe src="/mus/3_dushambe/index.html" width="100%" height="1470" frameborder="0"></iframe>
