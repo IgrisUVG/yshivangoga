f0b7c83e44ca559ddee13783909292fcd544fe77
IgrisUVG <igris@ushivangoga.ru>
2017-05-05 11:51:13 +0300

    Плейлист 12_hp.

---
+++ mus/12_hp/playlist_12.js
@@ -0,0 +1,98 @@
+//<![CDATA[
+$(document).ready(function () {
+
+    new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    }, [
+        {
+            title: "ШАПКАСУШАМИ СНАМИ",
+            //free: true,
+            mp3: "12_01.mp3"
+        },
+        {
+            title: "ПАРТИЗАНЫ-ФРИКИ",
+            //free: true,
+            mp3: "12_02.mp3"
+        },
+        {
+            title: "ГЛАВНЫЕ ТАПОЧКИ",
+            //free: true,
+            mp3: "12_03.mp3"
+        },
+        {
+            title: "БИЛИБЕРДА",
+            //free: true,
+            mp3: "12_04.mp3"
+        },
+        {
+            title: "ДОМ БЕЗ КРЫШИ",
+            //free: true,
+            mp3: "12_05.mp3"
+        },
+        {
+            title: "ВРЕМЯ МЕДЛЕННЫХ ТАНЦЕВ",
+            //free: true,
+            mp3: "12_06.mp3"
+        },
+        {
+            title: "ФРОЙЛЕН",
+            //free: true,
+            mp3: "12_07.mp3"
+        },
+        {
+            title: "ФИГЕЙРО",
+            //free: true,
+            mp3: "12_08.mp3"
+        },
+        {
+            title: "АТУ-АТУ ФУФЫРИКИ",
+            //free: true,
+            mp3: "12_09.mp3"
+        },
+        {
+            title: "СЛУШАЯ МАМОНОВА",
+            //free: true,
+            mp3: "12_10.mp3"
+        },
+        {
+            title: "ДАУН-ДЭНС",
+            //free: true,
+            mp3: "12_11.mp3"
+        },
+        {
+            title: "ТУполев",
+            //free: true,
+            mp3: "12_12.mp3"
+        },
+        {
+            title: "ЬТАВОЛАЖОП ОРБОД",
+            //free: true,
+            mp3: "12_13.mp3"
+        },
+        {
+            title: "ПОРОСЁНОК, ХОМЯЧОК И РЕДКАЯ ПТИЦА",
+            //free: true,
+            mp3: "12_14.mp3"
+        },
+        {
+            title: "РОМШТЕКС",
+            //free: true,
+            mp3: "12_15.mp3"
+        },
+        {
+            title: "ОСЛЫ ТОЛЬКО ЗНАЮТ",
+            //free: true,
+            mp3: "12_16.mp3"
+        }
+    ], {
+        swfPath: "js/jplayer",
+        supplied: "oga, mp3",
+        wmode: "window",
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true
+    });
+});
+//]]>
