1077c4ac746cf3e5097c9f0a07f1bab49fe51bb8
IgrisUVG <igris@ushivangoga.ru>
2017-05-07 18:23:56 +0300

    Попытка добавления иконки

--- data/head.php
+++ data/head.php
@@ -1,5 +1,4 @@
 <title>УШИ ВАН ГОГА || ОФИЦИАЛЬНЫЙ САЙТ</title>
-<link rel="icon" href="uho_m.png" type="image/png">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script type="text/javascript" src="/js/menu.js"></script>
@@ -7,6 +6,7 @@
 <!--[if lte IE 8]>
 <script src="/js/respond.min.js"></script>
 <![endif]-->
+<link rel="icon" href="/img/uho_m.png" type="image/png">
 <link href="/css/reset.css" rel="stylesheet" type="text/css"/>
 <link href="/css/style.css" rel="stylesheet" type="text/css"/>
 <!--link href="css/menuLstyle.css" rel="stylesheet" type="text/css" /-->
