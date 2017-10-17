3394811c8133e816a3038c64699d4d27a4c51b95
IgrisUVG <igris@ushivangoga.ru>
2017-10-17 16:55:35 +0300

    suppe change

--- suppe/suppe.js
+++ suppe/suppe.js
@@ -15,8 +15,11 @@ var suppe = [
     'С фрикадельками',
     'Куриный'
 ];
+var result = '';
+
 function findSuppe() {
     var min = 1, max = suppe.length;
     var i = ((Math.floor(Math.random() * (max - min + 1)) + min)) - 1;
-    document.getElementById('res').innerHTML = suppe[i];
+    result = suppe[i];
+    document.getElementById('res').innerHTML = result;
 }
