1eac248e39a79da1a7c5fedc70f0ad0f92776d5a
IgrisUVG <igris@ushivangoga.ru>
2019-07-05 11:31:01 +0300

    Radio styles ++

--- data/head.php
+++ data/head.php
@@ -1,7 +1,8 @@
 ﻿<title>УШИ ВАН ГОГА || ОФИЦИАЛЬНЫЙ САЙТ</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<link rel="icon" href="uho_m.png" type="image/png"/>
+<!--<link rel="icon" href="uho_m.png" type="image/png"/>-->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script type="text/javascript" src="/js/menu.js"></script>
 <script type="text/javascript" src="/js/script.js"></script>
