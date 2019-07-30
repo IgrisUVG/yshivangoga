73a235e6743891531af9808963d7bf759911aaa7
IgrisUVG <igris@ushivangoga.ru>
2019-07-30 18:12:57 +0300

    Suppe suche js?

--- suppe/suppe.js
+++ suppe/suppe.js
@@ -52,6 +52,37 @@ function findSuppe() {
     })
 }
 
+function listSuppe() {
+    var ul = document.createElement('ul');
+    for (i in suppe) {
+        result = suppe[i];
+        links = ingred[i];
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        $(a).appendTo(li);
+        $(a).text(result);
+        $(a).attr('href', ingred[i]);
+        $(li).appendTo(ul);
+    }
+    $(ul).appendTo($('#res_list'));
+}
+
+// function showSuppe() {
+//     $(ul).hide(500, 'swing');
+//     $('#butLoupe').animate({
+//         fontSize: 'hide',
+//         width: 'hide',
+//         height: 'hide',
+//         opacity: 'hide',
+//         display: 'none'
+//     }, 500, 'swing');
+//     document.getElementById('res').innerHTML = result;
+//     $('#res').animate({
+//         opacity: 1
+//     }, 500, 'swing');
+//     $('#ingred').load('ingredients.html ' + links + ' > *').show('slow');
+// }
+
 $('#but').click(function () {
     $(this).hide(500, 'swing');
     $('#butMain').animate({
@@ -91,4 +122,26 @@ $('#res').click(function () {
         opacity: 0
     }, 500, 'swing');
     $('#ingred').hide('fast')
+});
+
+$('#butLoupe').click(function () {
+    $(this).hide(500, 'swing');
+    $('#but').hide(500, 'swing');
+    $('#search').animate({
+        width: 'hide',
+        height: 'hide',
+        opacity: 'hide',
+        display: 'none'
+    }, 500, 'swing', listSuppe());
+    $('#butMain').animate({
+        fontSize: 'hide',
+        width: 'hide',
+        height: 'hide',
+        opacity: 'hide',
+        display: 'none'
+    }, 500, 'swing');
+    $('#res_list').animate({
+        display: 'block',
+        opacity: 1
+    }, 500, 'swing');
 });
